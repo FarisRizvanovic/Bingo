e61956d8e2754c74af5767464580f3005ef197e4
Faris Rizvanovic <faris.rizvanovic00@gmail.com>
2023-04-30 18:09:44 +0200

    Initial commit

---
+++ Bingo/BingoUtil.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Bingo
+{
+    // Ako smatrate da nesto fali napisite u komentar issue-a
+    public static class BingoUtil
+    {
+        /**
+         * Generisati listice koristeci GenerisiKombinacijuZaListic()
+         */
+        public static List<List<int>> GenerisiListice(int zeljeniBrojListica)
+        {
+            List<List<int>> listici = new List<List<int>>();
+
+            return listici;
+        }
+
+        /**
+         * Generisati kombinaciju od 6 brojeva (1-39) 
+         */
+        public static List<int> GenerisiKombinacijuZaListic()
+        {
+            List<int> kombinacija = new List<int>();
+
+            return kombinacija;
+        }
+
+        /**
+         * Generisane listice spremiti u .txt fajl
+         */
+        public static void SpremiListice(List<List<int>> listici, string nazivFajlaSaExtenzijom)
+        {
+
+        }
+
+        /**
+         * Generisane listice koji su spremljeni u .txt fajlu procitati
+         */
+        public static List<List<int>> ProcitajListice()
+        {
+            List<List<int>> listici = new List<List<int>>();
+
+
+            return listici;
+        }
+
+        /**
+         * Izvuci pobjednike i generisati izvjestaje
+         * Izvjestaje spremiti u .txt fajl
+         * Spremanje izvjestaja uraditi sa SpremanjeIzvjestaja()
+         * Ispis izvrsti sa Ispis()
+         */
+        public static void GenerisanjeIzvjestaja()
+        {
+
+        }
+
+        /**
+         * Napraviti jednostavan ispis
+         */
+        static void Ispis(string stringZaIspisati)
+        {
+
+        }
+
+        /**
+         * Spremiti izvjestaje
+         */
+        static void SpremanjeIzvjestaja(string izvjestaj)
+        {
+
+        }
+    }
+}
+
--- Bingo/Program.cs
+++ Bingo/Program.cs
@@ -1,3 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Bingo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LOTO!!");
 
+            // Generisanje
+            Generisanje();
+
+            // Izvlacenje
+            Izvlacenje();
+
+            Console.ReadKey();
+        }
+
+        static void Generisanje()
+        {
+            // Traziti unos kolko listica zelimo
+            Console.WriteLine("Unesite broj listica za generisati: ");
+            int zeljeniBrojListica = Convert.ToInt32(Console.ReadLine());
+
+            // Generisati listice
+            List<List<int>> listici = BingoUtil.GenerisiListice(zeljeniBrojListica);
+
+            // Spremiti listice u fajl
+            BingoUtil.SpremiListice(listici, "listici.txt");
+
+        }
+
+
+        static void Izvlacenje()
+        {
+            // Generisati pobjednicku kombinaciju
+            List<int> pobjednickaKobinacija = BingoUtil.GenerisiKombinacijuZaListic();
+
+            // Procitati listice iz fajla
+            List<List<int>> listici = BingoUtil.ProcitajListice();
+
+            // Generisanje izvjestaja i ispis
+            BingoUtil.GenerisanjeIzvjestaja();
+
+        }
+
+    }
+
+}
